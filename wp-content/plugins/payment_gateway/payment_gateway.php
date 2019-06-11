cf3362091c824c895824bdbe6c9647d7044d42db
nina8604 <nina.yaremenko86@gmail.com>
2019-06-11 15:17:23 +0300

    define settings fields

--- wp-content/plugins/payment_gateway/payment_gateway.php
+++ wp-content/plugins/payment_gateway/payment_gateway.php
@@ -65,8 +65,53 @@ function stripe_init_gateway_class() {
 		 * You should define settings fields
 		 */
 		public function init_form_fields(){
+			$this->form_fields = array(
+				'enabled' => array(
+					'title'       => 'Enable/Disable',
+					'label'       => 'Enable Stripe Gateway',
+					'type'        => 'checkbox',
+					'description' => '',
+					'default'     => 'no',
+				),
+				'title' => array(
+					'title'       => 'Title',
+					'type'        => 'text',
+					'description' => 'This controls the title which the user sees during checkout',
+					'default'     => 'Credit Card',
+					'desc_tip'    => true,
+				),
+				'description' => array(
+					'title'       => 'Description',
+					'type'        => 'textarea',
+					'description' => 'This controls the description which the user sees during checkout.',
+					'default'     => 'Pay with your credit card',
+				),
+				'testmode' => array(
+					'title'       => 'Test mode',
+					'label'       => 'Enable Test Mode',
+					'type'        => 'checkbox',
+					'description' => 'Place the payment gateway in test mode using test API keys.',
+					'default'     => 'yes',
+					'desc_tip'    => true,
+				),
+				'test_publishable_key' => array(
+					'title'       => 'Test Publishable Key',
+					'type'        => 'text'
+				),
+				'test_private_key' => array(
+					'title'       => 'Test Private Key',
+					'type'        => 'password',
+				),
+				'publishable_key' => array(
+					'title'       => 'Live Publishable Key',
+					'type'        => 'text'
+				),
+				'private_key' => array(
+					'title'       => 'Live Private Key',
+					'type'        => 'password'
+				)
+			);
 
-			...
 
 		}
 
